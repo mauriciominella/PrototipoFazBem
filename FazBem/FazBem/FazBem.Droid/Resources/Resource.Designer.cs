f645593de89e6f482c0b684e7db72be1eedeb5ba
Rodrigo Wirth <rodrigowirth90@gmail.com>
2015-09-13 11:41:21 -0300

    Adding fisrt version of camera view

--- FazBem/FazBem/FazBem.Droid/Resources/Resource.Designer.cs
+++ FazBem/FazBem/FazBem.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace FazBem.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::XLabs.Platform.Resource.String.ApplicationName = global::FazBem.Droid.Resource.String.ApplicationName;
+			global::XLabs.Platform.Resource.String.Hello = global::FazBem.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -60,11 +62,30 @@ namespace FazBem.Droid
 			}
 		}
 		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
+			{
+			}
+		}
+		
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Mono_Android_Theme_Splash = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Mono_Android_Theme_Splash = 2130968576;
 			
 			static Style()
 			{
