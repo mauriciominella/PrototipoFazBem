68435dfefa58a0f44a993956fefa7a0a7a79a7a7
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2015-09-13 10:52:36 -0300

    ProductDetailViewModel added to the project

---
+++ FazBem/FazBem/FazBem/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FazBem.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FazBem.ViewModels
+{
+    public class ProductDetailViewModel : ViewModelBase
+    {
+        public ObservableCollection<UserRating> Ratings { get; set; }
+
+        public Product ProductToBeRated { get; set; }
+
+        public ICommand LikeCommand { get; set; }
+
+        public ICommand UnlinkeCommand { get; set; }
+
+    }
+}
