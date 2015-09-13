f91bec54ddf835860e97a0e8e396c9512edcd481
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2015-09-13 10:24:10 -0300

    Basic entities created.

---
+++ FazBem/FazBem/FazBem/Models/EnumProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FazBem.Models
+{
+    public enum EnumProfile
+    {
+        Lactose = 1,
+        Gluten = 2,
+        Vegano = 3
+    }
+}
