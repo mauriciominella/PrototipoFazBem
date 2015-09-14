245b1db19074a3fcab3bb8712d31a91b2e3ef21f
Rodrigo Wirth <rodrigowirth90@gmail.com>
2015-09-14 14:45:31 -0300

    Setting IOS configurations

--- FazBem/FazBem/FazBem.iOS/AppDelegate.cs
+++ FazBem/FazBem/FazBem.iOS/AppDelegate.cs
@@ -4,6 +4,10 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using XLabs.Ioc;
+using XLabs.Platform.Device;
+using XLabs.Platform.Services;
+using FazBem.Interfaces;
 
 namespace FazBem.iOS
 {
@@ -22,6 +26,17 @@ namespace FazBem.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            #region Resolver Init
+
+            SimpleContainer container = new SimpleContainer();
+            container.Register<IDevice>(t => AppleDevice.CurrentDevice);
+            container.Register<IDisplay>(t => t.Resolve<IDevice>().Display);
+            container.Register<INetwork>(t => t.Resolve<IDevice>().Network);
+            container.Register<ISQLite>(typeof(SQLiteAndroid));
+
+            Resolver.SetResolver(container.GetResolver());
+            #endregion
+
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
---
+++ FazBem/FazBem/FazBem.iOS/Implementations/SQLiteIOS.cs
@@ -0,0 +1,32 @@
+﻿using FazBem.Interfaces;
+using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FazBem.iOS.Implementations
+{
+    public class SQLiteIOS : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "fazbem.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder instead
+            var path = Path.Combine(libraryPath, sqliteFilename);
+
+            // This is where we copy in the prepopulated database
+            Console.WriteLine(path);
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            var conn = new SQLiteConnection(new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS(), path);
+
+            // Return the database connection 
+            return conn;
+        }
+    }
+}
